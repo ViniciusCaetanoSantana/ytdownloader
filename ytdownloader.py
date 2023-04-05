b4a41e1989283339dfb3c856d85a7dfb9d55b190
ViniciusCaetanoSantana <vinicius.caetano.santana@gmail.com>
2023-04-05 00:29:36 -0300

    Update ytdownloader.py

--- ytdownloader.py
+++ ytdownloader.py
@@ -6,53 +6,70 @@ from pytube import Playlist
 import os
 
 num = input('Quantos links?')
-
 caminho = os.getcwd()
-file = caminho + r"\coisas baixadas(pytube)"
+arquivo = caminho + r"\coisas baixadas(pytube)"
 
+links_download = []
+tipo_download=[]
 try:
     for baixar in range(int(num)):
+
         links = str(input('Coloque o link'))
+        links_download.append(links)
+
         tipo = str(input('Tipo (MP3/MP4)')).lower()
+        tipo_download.append(tipo)
 
-        if 'playlist' in links:
+    for i, videoss in enumerate(links_download):
+            
+        if 'playlist' in videoss:
             try:
-                p = Playlist(f'{links}')
-
+                p = Playlist(videoss)
+                    
             except exceptions.RegexMatchError:
-                print(f'LINK NÃO ACEITO, VOCÊ ESCREVEU: {links}')
+                print(f'LINK NÃO ACEITO, VOCÊ ESCREVEU: {videoss}')
                 break
 
             print(f'Baixando playlist "{p.title}":')
-
             for video in p.videos:
-                if tipo == 'mp4':
-                    video.streams.filter(res='720p').first().download(output_path=file)
-                    print(f'{video.title}.{tipo} baixado')
-                if tipo == 'mp3':
-                    video.streams.filter(only_audio=True).first().download(output_path=file)
-                    print(f'{video.title}.{tipo} baixado')
-                elif tipo != 'mp4' and tipo != 'mp3':
-                    print(f'ESCREVA MP3 OU MP4, VOCÊ ESCREVEU: {tipo}')
+
+                if tipo_download[i] == 'mp4':
+                    video.streams.filter(res='720p').first().download(output_path=arquivo)
+                    print(f'{video.title}.{tipo_download[i]} na lista de download')
+
+
+                if tipo_download[i] == 'mp3':
+                    video.streams.filter(only_audio=True).first().download(output_path=arquivo)
+                    print(f'{video.title}.{tipo_download[i]} na lista de download')
+
 
         
+                elif tipo_download[i] != 'mp4' and tipo_download[i] != 'mp3':
+                    print(f'ESCREVA MP3 OU MP4, VOCÊ ESCREVEU: {tipo_download[i]}')
+
+            
 
         else:
             try:
-                yt = YouTube(links)
-                
+                yt = YouTube(videoss)
+
             except exceptions.RegexMatchError:
-                print(f'LINK NÃO ACEITO, VOCÊ ESCREVEU: {links}')
+                print(f'LINK NÃO ACEITO, VOCÊ ESCREVEU: {videoss}')
                 break
 
-            if tipo == 'mp4':
-                yt.streams.filter(res='720p').first().download(output_path=file)
-                print(f'{yt.title}.{tipo} baixado')
-            elif tipo == 'mp3':
-                yt.streams.filter(only_audio=True).first().download(output_path=file)
-                print(f'{yt.title}.{tipo} baixado')           
-            elif tipo != 'mp4' and tipo != 'mp3':
-                print(f'ESCREVA MP3 OU MP4, VOCÊ ESCREVEU: {tipo}')
+            if tipo_download[i] == 'mp4':
+                videos = yt.streams.filter(res='720p')
+                print(f'{yt.title}.{tipo_download[i]} na lista de download')
+                videos.first().download(output_path=arquivo)
+
+            elif tipo_download[i] == 'mp3':
+                videos = yt.streams.filter(only_audio=True)
+                print(f'{yt.title}.{tipo_download[i]} na lista de download')
+                videos.first().download(output_path=arquivo)   
+
+            elif tipo_download[i] != 'mp4' and tipo_download[i] != 'mp3':
+                print(f'ESCREVA MP3 OU MP4, VOCÊ ESCREVEU: {tipo_download[i]}')
+
 
 except ValueError:
     print(f'NÚMERO DE LINKS NÃO ACEITO, VOCÊ ESCREVEU: {num}')
@@ -62,5 +79,7 @@ except KeyError:
     print(f'O LINK DIRECIONA A ALGO PRIVADO, COLOQUE-O EM NÃO LISTADO PARA CONSEGUIR BAIXAR!!!')
     pass
 
+
+print('\nDOWNLOAD FEITO!!')
 print('\n')
 print('-PROGRAMA FINALIZADO-\n'*2)
